d647da72676c0eb7f4f6fee001870e9f8fb7af1d
polamjag <s@polamjag.info>
2014-04-14 02:06:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# mikutter base64 plugin
+
+## tip
+you should bind key to "encode as base64" from settings
+
+## License
+MIT
---
+++ mikutter_base64.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+require 'base64'
+
+Plugin.create :mikutter_base64 do
+
+  command(:decode_base64,
+          name: "このツイートを base64 としてデコード",
+          condition: Plugin::Command[:HasMessage],
+          visible: true,
+          role: :timeline) do |target|
+    
+    target.messages.each do |mes|
+      begin
+        p mes.message
+        p mes.user
+        md = Gtk::MessageDialog.new(nil, Gtk::Dialog::DESTROY_WITH_PARENT, Gtk::MessageDialog::WARNING, 
+                                    Gtk::MessageDialog::BUTTONS_CLOSE, ((t = Base64.decode64(mes[:message])) == "") ? "not valid base64" : t)
+        md.run
+        md.destroy
+      rescue => err
+        p err
+      end
+    end
+  
+  end
+
+  command(:encode_base64,
+          name: "現在の Postbox の内容を base64 でエンコード",
+          condition: lambda { |opt| true },
+          visible: true,
+          role: :postbox) do |opt|
+    box = Plugin[:gtk].widgetof(opt.widget).widget_post.buffer
+    box.text = Base64.encode64(box.text)
+  end
+
+end
